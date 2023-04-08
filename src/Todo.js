c1d1914b8d627ae4f385a36277fe719ba7eb97cc
YANGSEOKWOO <didtjrdn030@naver.com>
2023-04-08 23:30:17 +0900

    미완료 목록 추가

--- src/Todo.js
+++ src/Todo.js
@@ -23,18 +23,26 @@ function Todo() {
     setItems(newItems);
   }
 
+  function toggleIncomplete(index) {
+    const newItems = [...items];
+    newItems[index].incomplete = !newItems[index].incomplete;
+    setItems(newItems);
+  }
+
   function deleteItem(index) {
     const newItems = [...items];
     newItems.splice(index, 1);
     setItems(newItems);
   }
 
+  const incompleteItems = items.filter(item => !item.completed);
+
   return (
     <div className="todo-container">
       <h1 className="todo-title">Todo List</h1>
       <ul className="todo-list">
         {items.map((item, index) => (
-          <li key={index} className={item.completed ? 'todo-item todo-completed' : 'todo-item'}>
+          <li key={index} className={item.completed ? 'todo-item todo-completed' : item.incomplete ? 'todo-item todo-incomplete' : 'todo-item'}>
             <input
               type="checkbox"
               checked={item.completed}
@@ -43,6 +51,14 @@ function Todo() {
             />
             <span className="todo-text">{item.text}</span>
             <button onClick={() => deleteItem(index)} className="todo-delete-btn">Delete</button>
+            {!item.completed && (
+              <input
+                type="checkbox"
+                checked={item.incomplete}
+                onChange={() => toggleIncomplete(index)}
+                className="todo-checkbox"
+              />
+            )}
           </li>
         ))}
       </ul>
@@ -50,6 +66,18 @@ function Todo() {
         <input type="text" name="newItem" className="todo-input" />
         <button type="submit" className="todo-add-btn">Add</button>
       </form>
+      {incompleteItems.length > 0 && (
+        <div className="todo-incomplete">
+          <h2> 완료 못한 {incompleteItems.length} 개의 목록</h2>
+          <ul className="todo-incomplete-list">
+            {incompleteItems.map((item, index) => (
+              <li key={index} className="todo-incomplete-item">
+                {item.text}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
