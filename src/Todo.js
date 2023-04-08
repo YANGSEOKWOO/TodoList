37d3969be2d9e8d989c92b1439e64ddc11c3214d
YANGSEOKWOO <didtjrdn030@naver.com>
2023-04-08 23:34:18 +0900

    날짜 추가

--- src/Todo.js
+++ src/Todo.js
@@ -4,15 +4,16 @@ import './Todo.css';
 function Todo() {
   const [items, setItems] = useState([]);
 
-  function addItem(text) {
-    setItems([...items, { text, completed: false }]);
+  function addItem(text, date) {
+    setItems([...items, { text, completed: false, date }]);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     const text = e.target.elements.newItem.value.trim();
     if (text) {
-      addItem(text);
+      const date = new Date().toLocaleDateString();
+      addItem(text, date);
       e.target.elements.newItem.value = '';
     }
   }
@@ -36,39 +37,47 @@ function Todo() {
   }
 
   const incompleteItems = items.filter(item => !item.completed);
+  const dates = [...new Set(items.map(item => item.date))];
 
   return (
     <div className="todo-container">
       <h1 className="todo-title">Todo List</h1>
-      <ul className="todo-list">
-        {items.map((item, index) => (
-          <li key={index} className={item.completed ? 'todo-item todo-completed' : item.incomplete ? 'todo-item todo-incomplete' : 'todo-item'}>
-            <input
-              type="checkbox"
-              checked={item.completed}
-              onChange={() => toggleComplete(index)}
-              className="todo-checkbox"
-            />
-            <span className="todo-text">{item.text}</span>
-            <button onClick={() => deleteItem(index)} className="todo-delete-btn">Delete</button>
-            {!item.completed && (
-              <input
-                type="checkbox"
-                checked={item.incomplete}
-                onChange={() => toggleIncomplete(index)}
-                className="todo-checkbox"
-              />
-            )}
-          </li>
-        ))}
-      </ul>
+      {dates.map(date => (
+        <div key={date}>
+          <h2 className="todo-date">{date}</h2>
+          <ul className="todo-list">
+            {items
+              .filter(item => item.date === date)
+              .map((item, index) => (
+                <li key={index} className={item.completed ? 'todo-item todo-completed' : item.incomplete ? 'todo-item todo-incomplete' : 'todo-item'}>
+                  <input
+                    type="checkbox"
+                    checked={item.completed}
+                    onChange={() => toggleComplete(index)}
+                    className="todo-checkbox"
+                  />
+                  <span className="todo-text">{item.text}</span>
+                  <button onClick={() => deleteItem(index)} className="todo-delete-btn">Delete</button>
+                  {!item.completed && (
+                    <input
+                      type="checkbox"
+                      checked={item.incomplete}
+                      onChange={() => toggleIncomplete(index)}
+                      className="todo-checkbox"
+                    />
+                  )}
+                </li>
+              ))}
+          </ul>
+        </div>
+      ))}
       <form onSubmit={handleSubmit}>
         <input type="text" name="newItem" className="todo-input" />
         <button type="submit" className="todo-add-btn">Add</button>
       </form>
       {incompleteItems.length > 0 && (
         <div className="todo-incomplete">
-          <h2> 완료 못한 {incompleteItems.length} 개의 목록</h2>
+          <h2>You still have {incompleteItems.length} incomplete items:</h2>
           <ul className="todo-incomplete-list">
             {incompleteItems.map((item, index) => (
               <li key={index} className="todo-incomplete-item">
